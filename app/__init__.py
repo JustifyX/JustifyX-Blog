d595d5096eae65202ec4044ee3d54e80622e5374
JustifyX <121234533+JustifyX@users.noreply.github.com>
2023-12-31 19:41:55 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from ext import app
-from routes import home, about, blog_list, get_search_results, search_results, inject_search_form, ai_blog, login, register, load_user, logout, serve_static, blog_detail, add_comment
+from routes import home, about, blog_list, get_search_results, search_results, inject_search_form, login, register, load_user, logout, serve_static, blog_detail, add_comment
